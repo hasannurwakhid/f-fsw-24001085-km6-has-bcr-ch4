a2ddafcd70561d52e73735d7a0da2178ce8941c8
Hasan Nur Wakhid <hasannurwakhid@gmail.com>
2024-03-29 15:04:20 +0700

    p

--- route/index.js
+++ route/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const car = require("./car");
 const transmission = require("./transmission");
-
+const type = require("./type");
 
 router.use("/cars", car);
 router.use("/transmissions", transmission);
-
+router.use("/type", type);
 
 module.exports = router;
---
+++ route/type.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const typeController = require("../controller/type");
+
+router
+  .route("/")
+  .get(typeController.getTypes)
+  .post(typeController.createType);
+
+router
+  .route("/:id")
+  .get(typeController.getType)
+  .put(typeController.updateType)
+  .delete(typeController.deleteType);
+
+module.exports = router;
